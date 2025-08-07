afa0cca8c14a01a352219fd558582df065d976aa
Chemsdine <126870359+Chemsdine223@users.noreply.github.com>
2025-08-07 21:38:34 +0000

    Update fernet.dart

--- lib/src/algorithms/fernet.dart
+++ lib/src/algorithms/fernet.dart
@@ -2,7 +2,7 @@ part of encrypt;
 
 /// Wraps the Fernet Algorithm.
 class Fernet implements Algorithm {
-  final _maxClockSkew = 60;
+  final _maxClockSkew = 600;
 
   late final Key _signKey;
   late final Key _encryptionKey;
